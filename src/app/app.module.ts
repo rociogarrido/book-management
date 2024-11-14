0df9782d2851d776e7509462b26520258c3c86bb
Rocio Garrido <rocioganon@gmail.com>
2024-11-14 12:41:06 +0100

    feat: create a component and methods to dispatch actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BookReducer } from './books/book.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
