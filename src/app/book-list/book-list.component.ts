58cfa2e21b7c7815951ed847034d948eff31ab76
Rocio Garrido <rocioganon@gmail.com>
2024-11-15 12:24:06 +0100

    feat: load books from the store

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Book } from '../models/book';
 import { AddBook, RemoveBook } from '../books/book.actions';
+import { AppState } from '../app.state';
 
 @Component({
   selector: 'app-book-list',
@@ -9,7 +11,11 @@ import { AddBook, RemoveBook } from '../books/book.actions';
   styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent {
-  constructor(private store: Store<{ books: Book[] }>) {}
+  books$: Observable<Book[]>;
+
+  constructor(private store: Store<{ books: Book[] }>) {
+    this.books$ = store.pipe(select('books'));
+  }
 
   addBook(id: string, title: string, author: string) {
     this.store.dispatch(AddBook({ id, title, author }));
