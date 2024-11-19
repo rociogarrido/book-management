e1bff5aadbc6643b8302f17caeca8788d381e9e6
Rocio Garrido <rocioganon@gmail.com>
2024-11-19 10:53:05 +0100

    feat: register the eefect and use Angular Dev Tools to test the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { AppState } from './app.state';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './books/book.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, BookListComponent],
@@ -14,6 +17,8 @@ import { AppState } from './app.state';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot<AppState>({ book: BookReducer }),
+    EffectsModule.forRoot([BookEffects]),
+    StoreDevtoolsModule.instrument(),
   ],
   providers: [],
   bootstrap: [AppComponent],
