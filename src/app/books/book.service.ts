63f0488c404cc0b6d4a9114c2a6bc6e7882a0b6a
Rocio Garrido <rocioganon@gmail.com>
2024-11-19 10:14:12 +0100

    feat: create the effect

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -9,6 +9,9 @@ export class BookService {
   constructor() {}
 
   addBook(book: Book): Observable<Book> {
+    // const err = new Error('Error while adding a book')
+    // return throwError(() => err)
+
     return of(book);
   }
 }
